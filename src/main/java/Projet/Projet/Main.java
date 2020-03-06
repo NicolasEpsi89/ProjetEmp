fbccda24d0563fd149b57038f9368e479e033e9f
NicolasEpsi89 <nicolas.cosseni@hotmail.fr>
2020-03-06 09:14:08 +0100

    code developer + prime

--- src/main/java/Projet/Projet/Main.java
+++ src/main/java/Projet/Projet/Main.java
@@ -5,19 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			Developer Developer1 = new Developer("Nicolas","cosse", 1,3 );
-			Testeur Testeuse1 = new Testeur("Monier", "Cecile", 2, 150);
-			int Ds1 = Developer1.salaire;
-			int Ts1 = Testeuse1.salaire;
-			if(Ts1 < Ds1) {
-				System.out.println("");
+			Developer Developer1 = new Developer("Nicolas","cosse","Java","licence DevOps", 1,7 );
+			Developer Developer2 = new Developer("desChamp", "Didier", "C++", "Bts SN", 2, 3);
+					
+			Developer1.PrimeDev();
+			Developer1.CalculesalaireD();
+			Developer2.PrimeDev();
+			Developer2.CalculesalaireD();
+			
+			System.out.println(Developer1.NomEp+" "+ Developer1.PrenomEp +" "+"a toucher un salaire de :"+ Developer1.salaired );
+			System.out.println(Developer2.NomEp+" "+ Developer2.PrenomEp +" a toucher un salaire de :"+ Developer2.salaired );
+			
+			if(Developer1.salaired < Developer2.salaired) {
+				System.out.println(Developer1.NomEp +" " +Developer1.PrenomEp +" "+ "as moin travaillez que :"+" "+Developer2.NomEp+ " "+ Developer2.PrenomEp );
 			}
-			else if(Ts1 > Ds1) {
-				System.out.println("");
+			
+			else if(Developer1.salaired > Developer2.salaired) {
+				System.out.println(Developer1.NomEp +" " +Developer1.PrenomEp +" " +"as plus travaillez que :"+" "+Developer2.NomEp+ " "+ Developer2.PrenomEp );
 			}
 			else {
-				System.out.println("");
-			}		
-	}
-
+				System.out.println(Developer1.NomEp +" " +Developer1.PrenomEp +" " +"on fais le meme travaille :"+" "+Developer2.NomEp+ " "+ Developer2.PrenomEp );
+			}
+			
+		}
 }
+
