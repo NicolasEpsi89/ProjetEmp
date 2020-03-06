564c4159a1e81835520ed3b03064a1c9f75dd3dd
NicolasEpsi89 <56729413+NicolasEpsi89@users.noreply.github.com>
2020-03-06 09:46:49 +0100

    Add files via upload

---
+++ TestTesteur.java
@@ -0,0 +1,12 @@
+package Projet.Projet;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTesteur {
+	public void testtesteur () throws Exception{
+		System.out.println("le test testtesteur commence");
+		Testeur Testeuse1;
+//		assertTrue();
+	}
+
+}
