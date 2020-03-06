14ba4b79ca8a48cd43ded9d4d718b39dcd0edcc5
asilvagni <asilvagni@seineyonne.fr>
2020-03-06 09:05:01 +0100

    suppresion contenu main

--- src/main/java/Projet/Projet/Main.java
+++ src/main/java/Projet/Projet/Main.java
@@ -5,19 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			Developer Developer1 = new Developer("Nicolas","cosse", 1,3 );
-			Testeur Testeuse1 = new Testeur("Monier", "Cecile", 2, 150);
-			int Ds1 = Developer1.salaire;
-			int Ts1 = Testeuse1.salaire;
-			if(Ts1 < Ds1) {
-				System.out.println("");
-			}
-			else if(Ts1 > Ds1) {
-				System.out.println("");
-			}
-			else {
-				System.out.println("");
-			}		
+		
 	}
 
 }
--- src/main/java/Projet/Projet/Testeur.java
+++ src/main/java/Projet/Projet/Testeur.java
@@ -1,11 +1,17 @@
 package Projet.Projet;
 
 public class Testeur extends Employee {
-	int Nbheure, salaire;
+	int Nbheure, salaire, nbProjetTest;
+	
 
-	public Testeur(String nomEp, String prenomEp, int idEp, int Nbheure) {
+	public Testeur(String nomEp, String prenomEp, int idEp, int Nbheure, int nbprojetTest ) {
 		super(nomEp, prenomEp, idEp);
 		this.Nbheure = Nbheure;
-		this.salaire =12*Nbheure+200;
+		this.nbProjetTest = nbprojetTest;
+		this.salaire =12 * Nbheure+200;
+	}
+	
+	public void primeParProjetTest (int nbprojetTest) {
+		
 	}
 }
